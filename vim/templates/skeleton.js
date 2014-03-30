ee8dff55c189b3648c15462adf0a21246636fe45
niris <nirisix@gmail.com>
2014-03-30 16:50:32 +0800

    update vim template

---
+++ vim/templates/skeleton.js
@@ -0,0 +1,5 @@
+(function() {
+    "use strict";
+    var win = (0, eval)("this");
+
+})();
---
+++ vim/templates/skeleton.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def main():
+    print("Usage: {} [arguments]".format(sys.argv[0]))
+
+if __name__ == '__main__':
+    main()
