72a56955b8b628b7add694113f59ae5206315ac9
niris <nirisix@gmail.com>
2014-05-15 19:51:18 +0800

    vim | skeleton.py | add logger

--- vim/templates/skeleton.py
+++ vim/templates/skeleton.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="[%(asctime)s%(levelname)8s] %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 
