3720caa6ab56d6e28bd858b2a00bac44c7bdc5b0
niris <nirisix@gmail.com>
2014-07-20 14:16:47 +0800

    vim | py template | new logger format

--- vim/templates/skeleton.py
+++ vim/templates/skeleton.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     datefmt="%Y-%m-%d %H:%M:%S",
-    format="[%(asctime)s%(levelname)8s] %(message)s"
+    format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
