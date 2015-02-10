0d4952731f0623f0d6c6fafef63bd3588b26050d
niris <nirisix@gmail.com>
2015-02-10 14:20:16 +0800

    add git filter

--- git/git.mk
+++ git/git.mk
@@ -1,5 +1,6 @@
 .PHONY: git
 git:
-	rm -f ~/.gitconfig ~/.gitignore_global
+	rm -f ~/.gitconfig ~/.gitignore_global ~/.gitconfig.filter
 	ln -s `pwd`/git/gitconfig ~/.gitconfig
+	ln -s `pwd`/git/gitconfig.filter ~/.gitconfig.filter
 	ln -s `pwd`/git/gitignore_global ~/.gitignore_global
