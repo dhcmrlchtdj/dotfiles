d62b1b2f6296134a0ba0a3ba83b554de73b0b0b4
niris <nirisix@gmail.com>
2015-02-13 11:33:18 +0800

    add pipsi

--- pip/pip.mk
+++ pip/pip.mk
@@ -3,3 +3,8 @@ pip:
 	rm -rf ~/.pip
 	mkdir ~/.pip
 	ln -s `pwd`/pip/pip.conf ~/.pip/pip.conf
+
+
+.PHONY: pipsi
+pipsi:
+	curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python2
