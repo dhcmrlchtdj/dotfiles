cb48842ccd25356763fa6ba202581f48d4ecf686
niris <nirisix@gmail.com>
2015-03-30 10:22:51 +0800

    git crlf

--- git/git.mk
+++ git/git.mk
@@ -1,6 +1,10 @@
 .PHONY: git
 git:
-	rm -f ~/.gitconfig ~/.gitconfig.ignore ~/.gitconfig.filter
+	rm -f ~/.gitconfig
 	ln -s `pwd`/git/gitconfig ~/.gitconfig
+	rm -f ~/.gitconfig.excludes
+	ln -s `pwd`/git/gitconfig.excludes ~/.gitconfig.excludes
+	rm -f ~/.gitconfig.attributes
+	ln -s `pwd`/git/gitconfig.attributes ~/.gitconfig.attributes
+	rm -f ~/.gitconfig.filter
 	ln -s `pwd`/git/gitconfig.filter ~/.gitconfig.filter
-	ln -s `pwd`/git/gitconfig.ignore ~/.gitconfig.ignore
