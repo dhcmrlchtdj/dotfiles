53337ac73e677e01fb0ba4b13790d6a2f52003e4
niris <nirisix@gmail.com>
2015-09-16 12:49:23 +0800

    update eslint config

--- eslint/eslint.mk
+++ eslint/eslint.mk
@@ -1,5 +1,5 @@
 .PHONY: eslint
 eslint:
 	rm -rf ~/.eslintrc ~/.eslintignore
-	ln -s `pwd`/eslint/eslintrc ~/.eslintrc
+	ln -s `pwd`/eslint/eslintrc.json ~/.eslintrc
 	ln -s `pwd`/eslint/eslintignore ~/.eslintignore
