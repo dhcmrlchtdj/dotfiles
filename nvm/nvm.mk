d3e00aa6b767dbb77d2803ea07439d56b0ed02b8
niris <nirisix@gmail.com>
2015-10-08 15:18:30 +0800

    disable nvmrc

--- nvm/nvm.mk
+++ nvm/nvm.mk
@@ -4,4 +4,4 @@ nvm:
 	rm -rf ~/.nvm/nvm.sh ~/.nvmrc
 	curl https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh -o `pwd`/nvm/nvm.sh
 	ln -s `pwd`/nvm/nvm.sh ~/.nvm/nvm.sh
-	ln -s `pwd`/nvm/nvmrc ~/.nvmrc
+	#ln -s `pwd`/nvm/nvmrc ~/.nvmrc
