0cd40b272afc00858d0049f4a18103aefa9ae96b
niris <nirisix@gmail.com>
2015-10-31 14:37:06 +0800

    add nvmrc

--- nvm/nvm.mk
+++ nvm/nvm.mk
@@ -5,3 +5,6 @@ nvm:
 	curl -s 'https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh' -o `pwd`/nvm/nvm.sh
 	ln -s `pwd`/nvm/nvm.sh ~/.nvm/nvm.sh
 	#ln -s `pwd`/nvm/nvmrc ~/.nvmrc
+
+nvmrc:
+	ln -s `pwd`/nvm/nvmrc ~/.nvmrc
