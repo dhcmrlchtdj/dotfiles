d06be16eedfd1bc7fbfb6fecb6ad1401e2d6270f
niris <nirisix@gmail.com>
2015-11-05 12:22:40 +0800

    nvm

--- nvm/nvm.mk
+++ nvm/nvm.mk
@@ -4,8 +4,4 @@ nvm:
 	rm -rf ~/.nvm/nvm.sh ~/.nvmrc
 	curl -s 'https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh' -o `pwd`/nvm/nvm.sh
 	ln -s `pwd`/nvm/nvm.sh ~/.nvm/nvm.sh
-	#ln -s `pwd`/nvm/nvmrc ~/.nvmrc
-
-nvmrc:
-	rm -rf ~/.nvmrc
-	cp `pwd`/nvm/nvmrc ~/.nvmrc
+	if [ "Darwin" != `uname` ]; then ln -s `pwd`/nvm/nvmrc ~/.nvmrc; fi
