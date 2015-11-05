bde684b7ffe2415e7f0811a72485042a44f5f2f5
niris <nirisix@gmail.com>
2015-11-05 15:29:42 +0800

    change zsh url

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -1,7 +1,7 @@
 .PHONY: zsh
 zsh:
 	rm -rf ~/.zshrc ~/.zshrc_grml ~/.zshrc_prompt ~/.zshrc_others ~/.zshrc_common
-	curl -s 'http://git.grml.org/?p=grml-etc-core.git;a=blob_plain;f=etc/zsh/zshrc;hb=HEAD' -o `pwd`/zsh/zshrc_grml
+	curl 'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc' -o `pwd`/zsh/zshrc_grml
 	ln -s `pwd`/zsh/zshrc_grml ~/.zshrc_grml
 	ln -s `pwd`/zsh/zshrc_prompt ~/.zshrc_prompt
 	ln -s `pwd`/zsh/zshrc_common ~/.zshrc_common
