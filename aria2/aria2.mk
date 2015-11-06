12025d144bb706ced87c2f2330fafb90c8da3729
niris <nirisix@gmail.com>
2015-11-07 00:35:37 +0800

    aria2 config

--- aria2/aria2.mk
+++ aria2/aria2.mk
@@ -1,6 +1,6 @@
 .PHONY: aria2
 aria2:
 	rm -rf ~/.aria2
-	mkdir ~/.aria2
+	mkdir -p ~/.aria2 ~/tmp/aria2
 	touch ~/.aria2/aria2.session
 	sed "s@~@${HOME}@" `pwd`/aria2/aria2.conf > ~/.aria2/aria2.conf
