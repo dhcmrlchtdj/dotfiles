d40974f5e8e99de4c19c0fc8c5b22ed4d52d49a6
niris <nirisix@gmail.com>
2015-11-07 01:42:14 +0800

    aria | secret

--- aria2/aria2.mk
+++ aria2/aria2.mk
@@ -3,4 +3,6 @@ aria2:
 	rm -rf ~/.aria2
 	mkdir -p ~/.aria2 ~/tmp/aria2
 	touch ~/.aria2/aria2.session
-	sed "s@~@${HOME}@" `pwd`/aria2/aria2.conf > ~/.aria2/aria2.conf
+	sed -e "s@~@${HOME}@" \
+		-e "s@\<secret\>@`LC_CTYPE=C tr -dc '[:alnum:]' < /dev/urandom | head -c 16`@" \
+		`pwd`/aria2/aria2.conf > ~/.aria2/aria2.conf
