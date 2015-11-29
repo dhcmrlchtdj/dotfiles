a5e3a54d9021b7ddb383d0216bb015697a680a44
niris <nirisix@gmail.com>
2015-11-29 13:09:32 +0800

    update htop

--- htop/htop.mk
+++ htop/htop.mk
@@ -1,5 +1,8 @@
 .PHONY: htop
 htop:
 	rm -rf ~/.htoprc
-	ln -s `pwd`/htop/htoprc ~/.htoprc
+	cp `pwd`/htop/htoprc ~/.htoprc
+	mkdir -p ~/.config/htop
+	rm -rf ~/.config/htop/htoprc
+	ln -s `pwd`/htop/htoprc ~/.config/htop/htoprc
 	@echo
