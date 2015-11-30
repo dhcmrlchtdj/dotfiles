d844b80bac8e782c74d9d1a85d0cbfc88cc7afee
niris <nirisix@gmail.com>
2015-11-30 13:37:07 +0800

    htop osx detect

--- htop/htop.mk
+++ htop/htop.mk
@@ -1,8 +1,14 @@
+OS := $(shell uname)
+
 .PHONY: htop
 htop:
-	rm -rf ~/.htoprc
-	cp `pwd`/htop/htoprc ~/.htoprc
+ifeq ($(OS),Linux)
 	mkdir -p ~/.config/htop
 	rm -rf ~/.config/htop/htoprc
 	ln -s `pwd`/htop/htoprc ~/.config/htop/htoprc
+endif
+ifeq ($(OS),Darwin)
+	rm -rf ~/.htoprc
+	cp `pwd`/htop/htoprc ~/.htoprc
+endif
 	@echo
