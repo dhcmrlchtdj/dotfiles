148526d773a62247740b37cff6d52462ba3d0d02
niris <nirisix@gmail.com>
2015-12-02 10:33:45 +0800

    remove iptables

--- iptables/iptables.rule.sh
+++
@@ -1,5 +0,0 @@
-iptables -N log-reject
-iptables -A log-reject -m limit --limit 5/m --limit-burst 10 -j LOG
-iptables -A log-reject -j REJECT --reject-with icmp-port-unreachable
-
-#iptables -P INPUT DROP
