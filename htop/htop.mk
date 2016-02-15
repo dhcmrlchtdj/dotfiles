484a92ad78327bad3b86bc1376473576b5da6810
niris <nirisix@gmail.com>
2016-02-15 10:32:18 +0800

    update htop

--- htop/htop.mk
+++ htop/htop.mk
@@ -1,9 +1,4 @@
 .PHONY: htop
 htop:
-ifeq ($(OS),Linux)
 	mkdir -p ~/.config/htop
 	$(LN) `pwd`/htop/htoprc ~/.config/htop/htoprc
-endif
-ifeq ($(OS),Darwin)
-	cp -v `pwd`/htop/htoprc ~/.htoprc
-endif
