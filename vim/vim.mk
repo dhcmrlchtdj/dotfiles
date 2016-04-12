2cd3c73f8310310b7c63d15859811bda180f5739
niris <nirisix@gmail.com>
2016-04-12 13:58:11 +0800

    vim | use dein

--- vim/vim.mk
+++ vim/vim.mk
@@ -3,7 +3,7 @@ vim:
 	$(LN) `pwd`/vim ~/.vim
 	$(LN) `pwd`/vim/vimrc ~/.vimrc
 	$(LN) `pwd`/vim/tern.json ~/.tern-project
-	if [ ! -d ~/.vim/bundle/neobundle.vim ]; then \
-		git clone https://github.com/Shougo/neobundle.vim \
-		~/.vim/bundle/neobundle.vim; \
+	if [ ! -d ~/.vim/bundle/repos/github.com/Shougo/dein.vim ]; then \
+		git clone https://github.com/Shougo/dein.vim \
+		~/.vim/bundle/repos/github.com/Shougo/dein.vim; \
 		fi
