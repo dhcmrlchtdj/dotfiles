9d746227e3117979d4822106531ddbec921c147e
niris <nirisix@gmail.com>
2016-04-26 12:12:02 +0800

    update dnsmasq

--- dnsmasq/dnsmasq.mk
+++ dnsmasq/dnsmasq.mk
@@ -1,11 +1,14 @@
-.PHONY: dnsmasq
-dnsmasq:
+.PHONY: dnsmasq dnsmasq.d
+
+dnsmasq.d:
 	mkdir -p `pwd`/dnsmasq/dnsmasq.d
-	sed -e "s@CONF_DIR@${PWD}/dnsmasq/dnsmasq.d@" \
-		`pwd`/dnsmasq/dnsmasq.conf > /usr/local/etc/dnsmasq.conf
 	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf' \
 		-o `pwd`/dnsmasq/dnsmasq.d/google.china.conf
 	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf' \
 		-o `pwd`/dnsmasq/dnsmasq.d/bogus-nxdomain.china.conf
 	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf' \
 		-o `pwd`/dnsmasq/dnsmasq.d/accelerated-domains.china.conf
+
+dnsmasq: dnsmasq.d
+	sed -e "s@CONF_DIR@${PWD}/dnsmasq/dnsmasq.d@" \
+		`pwd`/dnsmasq/dnsmasq.conf > /usr/local/etc/dnsmasq.conf
