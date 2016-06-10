78c2edc388fb17b943c606fb903a3c633a571b7f
niris <nirisix@gmail.com>
2016-06-11 07:14:31 +0800

    zsh | mk order

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -1,6 +1,6 @@
 .PHONY: zsh
 zsh:
-	curl 'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc' -o ~/.zshrc_grml
 	cp `pwd`/zsh/zshrc_prompt_ooxx ~/.zshrc_prompt_ooxx
 	cp `pwd`/zsh/zshrc_common ~/.zshrc_common
 	cp `pwd`/zsh/zshrc_`uname | tr '[:upper:]' '[:lower:]'` ~/.zshrc
+	curl 'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc' -o ~/.zshrc_grml
