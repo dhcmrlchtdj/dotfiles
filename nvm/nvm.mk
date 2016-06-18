cef934d4bd328d75ad4e1eb9d74f3ccc3819c84e
niris <nirisix@gmail.com>
2016-06-18 09:08:39 +0800

    take nvm back

--- nvm/nvm.mk
+++ nvm/nvm.mk
@@ -1,7 +1,8 @@
 .PHONY: nvm nvmrc
 nvm:
 	mkdir -p ~/.nvm
-	curl 'https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh' -o ~/.nvm/nvm.sh
+	curl 'https://raw.githubusercontent.com/creationix/nvm/v0.31.1/nvm.sh' | \
+		sed -e '/nvm_process_parameters /d' > ~/.nvm/nvm.sh
 
 nvmrc:
 	cp `pwd`/nvm/nvmrc ~/.nvmrc
