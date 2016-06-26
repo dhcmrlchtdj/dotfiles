44d6663881aef57aa41f960363f7f65af4561066
nirisix <nirisix@gmail.com>
2016-06-26 17:52:28 +0800

    fix gnupg makefile

--- gnupg/gpg.mk
+++ gnupg/gpg.mk
@@ -1,6 +1,6 @@
 .PHONY: gpg
 gpg:
-	mkdir -p ~/.gnupg
+	mkdir -p -m 700 ~/.gnupg
 	-cp -n `pwd`/gnupg/gpg.conf ~/.gnupg/gpg.conf
 	-cp -n `pwd`/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 	-cp -n `pwd`/gnupg/dirmngr.conf ~/.gnupg/dirmngr.conf
