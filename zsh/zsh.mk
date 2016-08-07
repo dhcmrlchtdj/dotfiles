6a0f438bb08ad0428520a60ac70f625eae9d6904
niris <nirisix@gmail.com>
2016-08-07 11:21:46 +0800

    zsh | pyvenv

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -3,4 +3,5 @@ zsh:
 	-cp -n `pwd`/zsh/zshrc_prompt_ooxx ~/.zshrc_prompt_ooxx
 	-cp -n `pwd`/zsh/zshrc_common ~/.zshrc_common
 	-cp -n `pwd`/zsh/zshrc_`uname | tr '[:upper:]' '[:lower:]'` ~/.zshrc
+	-mkdir -p ~/.virtualenvwrapper
 	curl 'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc' -o ~/.zshrc_grml
