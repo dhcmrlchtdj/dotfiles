c8d0bdb934b65ec77136b479fc1af4a44554ce6a
niris <nirisix@gmail.com>
2016-08-08 11:26:29 +0800

    nvm | use master branch

--- nvm/nvm.mk
+++ nvm/nvm.mk
@@ -1,7 +1,7 @@
 .PHONY: nvm nvmrc
 nvm:
 	mkdir -p ~/.nvm
-	curl 'https://raw.githubusercontent.com/creationix/nvm/v0.31.1/nvm.sh' | \
+	curl 'https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh' | \
 		sed -e '/nvm_process_parameters /d' > ~/.nvm/nvm.sh
 
 nvmrc:
