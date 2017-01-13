82bd9afacdbabd43b40d79bf1495304b98ac4b4e
niris <nirisix@gmail.com>
2017-01-13 16:49:42 +0800

    zsh | update

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -5,3 +5,11 @@ zsh:
 	-cp -n `pwd`/zsh/zshrc_`uname | tr '[:upper:]' '[:lower:]'` ~/.zshrc
 	-mkdir -p ~/.virtualenvwrapper
 	curl 'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc' -o ~/.zshrc_grml
+
+bin_link:
+	-mkdir -p ~/.local/bin
+	-mkdir -p ~/.config/yarn/global/node_modules && \
+		ln -s ~/.local/bin ~/.config/yarn/global/node_modules/.bin
+ifeq ($(shell uname -s),Darwin)
+	-mkdir -p ~/Library/Python/3.6 && ln -s ~/.local/bin ~/Library/Python/3.6/bin
+endif
