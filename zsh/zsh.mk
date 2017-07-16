dc330780276923515327572b9e52cb6b8a470c52
niris <nirisix@gmail.com>
2017-07-16 14:41:57 +0800

    zsh | soruce

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -3,13 +3,4 @@ zsh:
 	-cp -n `pwd`/zsh/zshrc_prompt_ooxx ~/.zshrc_prompt_ooxx
 	-cp -n `pwd`/zsh/zshrc_common ~/.zshrc_common
 	-cp -n `pwd`/zsh/zshrc_`uname | tr '[:upper:]' '[:lower:]'` ~/.zshrc
-	-mkdir -p ~/.virtualenvwrapper
 	curl 'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc' -o ~/.zshrc_grml
-
-bin_link:
-	-mkdir -p ~/.local/bin
-	-mkdir -p ~/.config/yarn/global/node_modules && \
-		ln -s ~/.local/bin ~/.config/yarn/global/node_modules/.bin
-ifeq ($(shell uname -s),Darwin)
-	-mkdir -p ~/Library/Python/3.6 && ln -s ~/.local/bin ~/Library/Python/3.6/bin
-endif
