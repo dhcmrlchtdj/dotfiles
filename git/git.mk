7d38c0821b658e46f29b223f15fbc116854e5e2a
h11 <nirisix@gmail.com>
2018-11-27 23:00:19 +0800

    chore: ignore

--- git/git.mk
+++ git/git.mk
@@ -2,5 +2,5 @@
 git:
 	mkdir -p ~/.config/git
 	-cp -n `pwd`/git/gitconfig ~/.config/git/config
-	-cp -n `pwd`/git/gitconfig.ignore ~/.config/git/ignore
-	-cp -n `pwd`/git/gitconfig.attributes ~/.config/git/attributes
+	# -cp -n `pwd`/git/gitconfig.ignore ~/.config/git/ignore
+	# -cp -n `pwd`/git/gitconfig.attributes ~/.config/git/attributes
