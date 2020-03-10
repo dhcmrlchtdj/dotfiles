4bb2578e5dfb41b8417a4b5842c226abcf2a55d0
h11 <nirisix@gmail.com>
2020-03-10 13:06:14 +0800

    gpg

--- gnupg/gpg.mk
+++ gnupg/gpg.mk
@@ -1,8 +1,7 @@
 .PHONY: gpg
 gpg:
 	mkdir -p -m 700 ~/.gnupg
-	-cp -n `pwd`/gnupg/gpg.conf ~/.gnupg/gpg.conf
-	-cp -n `pwd`/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+	@# -cp -n `pwd`/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+	@# -curl 'https://sks-keyservers.net/sks-keyservers.netCA.pem' -o ~/.gnupg/sks-keyservers.netCA.pem
 	-cp -n `pwd`/gnupg/dirmngr.conf ~/.gnupg/dirmngr.conf
-	-curl 'https://sks-keyservers.net/sks-keyservers.netCA.pem' \
-		-o ~/.gnupg/sks-keyservers.netCA.pem
+	-cp -n `pwd`/gnupg/gpg.conf ~/.gnupg/gpg.conf
