0904fffac78eeab259f94b1faf26f2f43aaa0c2c
h11 <nirisix@gmail.com>
2020-03-11 11:03:04 +0800

    aria2

--- aria2/aria2.mk
+++ aria2/aria2.mk
@@ -1,13 +1,17 @@
 .PHONY: aria2 aria2service
 aria2:
-	mkdir -p ~/.aria2 ~/tmp/aria2
-	rm -rf ~/.aria2/aria2.session
-	touch ~/.aria2/aria2.session
-	rm -rf ~/.aria2/aria2.conf
+	mkdir -p ~/tmp
+	mkdir -p ~/.config/aria2
+	rm -rf ~/.config/aria2/aria2.session
+	touch ~/.config/aria2/aria2.session
+	rm -rf ~/.config/aria2/aria2.conf
 	sed -e "s@~@${HOME}@" \
-		-e "s@<secret>@`LC_CTYPE=C tr -dc '[:alnum:]' < /dev/urandom | head -c 16`@" \
-		`pwd`/aria2/aria2.conf > ~/.aria2/aria2.conf
+		-e "s@<secret>@`openssl rand -hex 8`@" \
+		`pwd`/aria2/aria2.conf > ~/.config/aria2/aria2.conf
 
 aria2service:
 	mkdir -p ~/.config/systemd/user/
 	-cp -n `pwd`/aria2/aria2.service ~/.config/systemd/user/
+
+aria2plist:
+	-cp -n `pwd/aria2/aria2.plist ~/Library/LaunchAgents/com.github.aria2.aria2.plist
