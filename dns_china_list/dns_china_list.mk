423f0bdc9d2e71a7a88b9c03e3e855bdbb4f271f
h11 <nirisix@gmail.com>
2020-03-12 13:39:13 +0800

    dns

--- dns_china_list/dns_china_list.mk
+++ dns_china_list/dns_china_list.mk
@@ -8,26 +8,23 @@
 .ONESHELL:
 china_list_mac:
 	cd dns_china_list
-	echo "----------"
 	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf' \
 		-o accelerated-domains.china.conf
 	gsed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' accelerated-domains.china.conf \
 		| egrep -v '^#' > accelerated-domains.china.raw.txt
 	gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
 		accelerated-domains.china.raw.txt > accelerated-domains.china.unbound.conf
-	echo "----------"
-	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf' \
-		-o google.china.conf
-	gsed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' google.china.conf \
-		| egrep -v '^#' > google.china.raw.txt
-	gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
-		google.china.raw.txt > google.china.unbound.conf
-	echo "----------"
-	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf' \
-		-o apple.china.conf
-	gsed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' apple.china.conf \
-		| egrep -v '^#' > apple.china.raw.txt
-	gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
-		apple.china.raw.txt > apple.china.unbound.conf
-	echo "----------"
-	cat *.txt | gsed -e 's|\(.*\)|- DOMAIN-SUFFIX,\1,DIRECT|' > clash.conf
+	# curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf' \
+	#     -o google.china.conf
+	# gsed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' google.china.conf \
+	#     | egrep -v '^#' > google.china.raw.txt
+	# gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
+	#     google.china.raw.txt > google.china.unbound.conf
+	# curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf' \
+	#     -o apple.china.conf
+	# gsed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' apple.china.conf \
+	#     | egrep -v '^#' > apple.china.raw.txt
+	# gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
+	#     apple.china.raw.txt > apple.china.unbound.conf
+	cat accelerated-domains.china.raw.txt \
+		| gsed -e 's|\(.*\)|  - DOMAIN-SUFFIX,\1,DIRECT|' > clash.conf
