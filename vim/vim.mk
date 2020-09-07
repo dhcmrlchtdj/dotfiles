4b61ba16b17da2e2d22d5e0334aedb4d4c2d3988
h11 <me@h11.io>
2020-09-07 16:20:15 +0800

    beancount vim

--- vim/vim.mk
+++ vim/vim.mk
@@ -1,16 +1,11 @@
-.PHONY: vim-config neovim vim
-
-vim-config:
-	-cp -n `pwd`/vim/conf/tern.json ~/.tern-project # tern
-	-cp -n `pwd`/vim/conf/clang-format ~/.clang-format # clang-format
-	-cp -n `pwd`/vim/conf/pep8 ~/.config/pep8 # autopep8
-	-cp -n `pwd`/vim/conf/flake8 ~/.config/flake8 # flake8
+.PHONY: neovim vim
 
 neovim:
 	mkdir -p ~/.config/nvim/{swap,backup,undo}
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	-cp -n `pwd`/vim/vimrc.vim ~/.config/nvim/init.vim
+	-cp -n -r `pwd`/vim/my-plugin/* ~/.config/nvim/
 
 vim: neovim
 	-ln -s ~/.config/nvim/init.vim ~/.vimrc
