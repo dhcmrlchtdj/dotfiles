b9869ed280754d5cb4fd5d7ac239d27513f05948
h11 <me@h11.io>
2020-09-22 10:05:06 +0800

    clash plist

--- proxy/proxy.mk
+++ proxy/proxy.mk
@@ -2,3 +2,6 @@
 proxychains:
 	mkdir -p ~/.proxychains
 	-cp -n `pwd`/proxychains/proxychains.conf ~/.proxychains/proxychains.conf
+
+# clash:
+#     cp ./clash.yaml ~/Library/LaunchAgents/com.github.dreamacro.clash.plist
