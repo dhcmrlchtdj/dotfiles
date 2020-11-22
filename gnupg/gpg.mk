cc12ad1a0a44ef58b848ab9e398a3fd228fbd16e
h11 <me@h11.io>
2020-11-22 16:34:56 +0800

    gpg

--- gnupg/gpg.mk
+++ gnupg/gpg.mk
@@ -1,7 +1,5 @@
 .PHONY: gpg
 gpg:
 	mkdir -p -m 700 ~/.gnupg
-	@# -cp -n `pwd`/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
-	@# -curl 'https://sks-keyservers.net/sks-keyservers.netCA.pem' -o ~/.gnupg/sks-keyservers.netCA.pem
 	-cp -n `pwd`/gnupg/dirmngr.conf ~/.gnupg/dirmngr.conf
 	-cp -n `pwd`/gnupg/gpg.conf ~/.gnupg/gpg.conf
