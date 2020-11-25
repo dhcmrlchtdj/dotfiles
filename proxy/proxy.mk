48fddb6b5b1ae039c6afb95079bfbe76d552695d
h11 <me@h11.io>
2020-11-25 11:42:22 +0800

    cleanup

--- proxy/proxy.mk
+++ proxy/proxy.mk
@@ -2,6 +2,3 @@
 proxychains:
 	mkdir -p ~/.proxychains
 	-cp -n `pwd`/proxychains/proxychains.conf ~/.proxychains/proxychains.conf
-
-# clash:
-#     cp ./clash.yaml ~/Library/LaunchAgents/com.github.dreamacro.clash.plist
--- vim/vim.mk
+++ vim/vim.mk
@@ -1,11 +1,9 @@
-.PHONY: neovim vim
+.PHONY: vim
 
-neovim:
+vim:
 	mkdir -p ~/.config/nvim/{swap,backup,undo}
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	-cp -n `pwd`/vim/vimrc.vim ~/.config/nvim/init.vim
 	-cp -n -r `pwd`/vim/my-plugin/* ~/.config/nvim/
-
-vim: neovim
 	-ln -s ~/.config/nvim/init.vim ~/.vimrc
