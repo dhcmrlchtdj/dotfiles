e08630c7aebae4c3a9cc49c141fd5179f149f635
h11 <me@h11.io>
2020-12-14 09:19:16 +0800

    china list

--- china_list/china_list.mk
+++ china_list/china_list.mk
@@ -1,22 +1,17 @@
 # https://github.com/felixonmars/dnsmasq-china-list
 
-# gsed -e "s|\(.*\)|\1 {\n  forward . 119.29.29.29\n}|" \
-#     china_list/accelerated-domains.china.raw.txt \
-#     > china_list/accelerated-domains.china.coredns.conf
-
 .PHONY: china_list
 china_list:
-	cd china_list
-	curl 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf' \
+	curl -L 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf' \
 		-o china_list/accelerated-domains.china.conf
 	gsed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' \
 		china_list/accelerated-domains.china.conf \
 		| egrep -v '^#' \
 		> china_list/accelerated-domains.china.raw.txt
+	gsed -e 's|\(.*\)|    - DOMAIN-SUFFIX,\1,DIRECT|' \
+		china_list/accelerated-domains.china.raw.txt \
+		> china_list/clash.conf
 	gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
 		china_list/accelerated-domains.china.raw.txt \
 		> china_list/unbound.conf
 	# cp china_list/unbound.conf /usr/local/etc/unbound/accelerated-domains.china.unbound.conf
-	gsed -e 's|\(.*\)|    - DOMAIN-SUFFIX,\1,DIRECT|' \
-		china_list/accelerated-domains.china.raw.txt \
-		> china_list/clash.conf
