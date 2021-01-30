6b4cffbbd67fc14c696cd705938af97a28e13fb5
h11 <me@h11.io>
2021-01-30 13:01:47 +0800

    china list | shunt

--- china_list/china_list.mk
+++ china_list/china_list.mk
@@ -11,7 +11,10 @@ china_list:
 	gsed -e 's|\(.*\)|    - DOMAIN-SUFFIX,\1,DIRECT|' \
 		china_list/accelerated-domains.china.raw.txt \
 		> china_list/clash.conf
-	gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
+	gsed -e 's|\(.*\)|"\1",|' \
 		china_list/accelerated-domains.china.raw.txt \
-		> china_list/unbound.conf
+		> china_list/shunt.conf
+	# gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
+	# 	china_list/accelerated-domains.china.raw.txt \
+	# 	> china_list/unbound.conf
 	# cp china_list/unbound.conf /usr/local/etc/unbound/accelerated-domains.china.unbound.conf
