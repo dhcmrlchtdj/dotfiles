dd892f0d8562cdedb386a15006dd42d6bd49695f
h11 <me@h11.io>
2021-02-14 10:22:50 +0800

    cleanup

--- dns/dns.mk
+++
@@ -1,5 +0,0 @@
-.PHONY: unbound
-unbound:
-	curl 'https://www.internic.net/domain/named.cache' \
-		-o '/usr/local/etc/unbound/root.hints'
-	-cp -n ./dns/unbound.conf /usr/local/etc/unbound/unbound.conf
