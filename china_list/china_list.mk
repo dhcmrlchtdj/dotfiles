f3c0f8830081c8517920112187d6688555fcd8e4
h11 <me@h11.io>
2021-05-02 06:52:34 +0800

    cleanup dns

--- china_list/china_list.mk
+++ china_list/china_list.mk
@@ -13,8 +13,4 @@ china_list:
 		> china_list/clash.conf
 	gsed -e 's|\(.*\)|"\1",|' \
 		china_list/accelerated-domains.china.raw.txt \
-		> china_list/shunt.conf
-	# gsed -e 's|\(.*\)|forward-zone:\n  name: "\1."\n  forward-addr: 119.29.29.29\n|' \
-	# 	china_list/accelerated-domains.china.raw.txt \
-	# 	> china_list/unbound.conf
-	# cp china_list/unbound.conf /usr/local/etc/unbound/accelerated-domains.china.unbound.conf
+		> china_list/dns.conf
