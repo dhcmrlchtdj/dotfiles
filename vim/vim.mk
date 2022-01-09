c0a910a064d8af289cba26fcb7ca7fe5379f0b1c
h11 <me@h11.io>
2022-01-09 18:04:57 +0800

    fix vim

--- vim/vim.mk
+++ vim/vim.mk
@@ -4,4 +4,5 @@ vim:
 	curl -f -L --create-dirs \
 		-o ~/.local/share/nvim/site/autoload/plug.vim \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	mkdir -p ~/.config/nvim
 	-cp -n `pwd`/vim/vimrc.vim ~/.config/nvim/init.vim
