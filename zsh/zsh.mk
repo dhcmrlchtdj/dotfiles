2096c9dec4d968ec0dc87627627399a51c58d54b
h11 <me@h11.io>
2022-01-11 20:58:41 +0800

    zsh | xdg

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -1,6 +1,10 @@
 .PHONY: zsh
 zsh:
-	-cp -n `pwd`/zsh/zshrc_prompt_ooxx ~/.zshrc_prompt_ooxx
-	-cp -n `pwd`/zsh/zshrc_common ~/.zshrc_common
+	mkdir -p ~/.config/zsh
+	mkdir -p ~/.local/state/zsh
+	mkdir -p ~/.local/share/zsh
+	mkdir -p ~/.cache/zsh
 	-cp -n `pwd`/zsh/zshrc_`uname | tr '[:upper:]' '[:lower:]'` ~/.zshrc
-	curl 'https://raw.githubusercontent.com/rupa/z/master/z.sh' -o ~/.zshrc_z
+	-cp -n `pwd`/zsh/zshrc_prompt_ooxx ~/.config/zsh/zshrc_prompt_ooxx
+	-cp -n `pwd`/zsh/zshrc_common ~/.config/zsh/zshrc_common
+	curl -sf 'https://raw.githubusercontent.com/rupa/z/master/z.sh' -o ~/.config/zsh/z.sh
