95ee71bc0fcfbb713f33446f97529b96bbc590ba
h11 <me@h11.io>
2022-09-09 20:47:58 +0800

    zsh | merge zshrc

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -4,7 +4,7 @@ zsh:
 	mkdir -p ~/.local/state/zsh
 	mkdir -p ~/.local/share/zsh
 	mkdir -p ~/.cache/zsh
-	-cp -n `pwd`/zsh/zshrc_`uname | tr '[:upper:]' '[:lower:]'` ~/.zshrc
+	-cp -n `pwd`/zsh/zshrc ~/.zshrc
 	-cp -n `pwd`/zsh/zshrc_prompt_ooxx ~/.config/zsh/zshrc_prompt_ooxx
 	-cp -n `pwd`/zsh/zshrc_common ~/.config/zsh/zshrc_common
 	curl -sf 'https://raw.githubusercontent.com/rupa/z/master/z.sh' -o ~/.config/zsh/z.sh
